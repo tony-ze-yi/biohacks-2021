46b9ab8fa425c97a47f157cb1fae67d4ab14d4f8
Tony (Ze) Yi <24402057+tony-ze-yi@users.noreply.github.com>
2021-03-13 17:07:34 -0500

    feat: parallelized optimized edit distance

--- main.py
+++ main.py
@@ -1,29 +1,103 @@
 import os
 import sys
+import time
 
 import numpy as np
-from numba import jit
+from numba import jit, njit, prange
+from numba import types
+from numba.typed import Dict, List
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "string1", type=str, help="path to first text file containing string"
-)
-parser.add_argument(
-    "string2", type=str, help="path to first text file containing string"
-)
 
-args = parser.parse_args()
-print(args.string1)
-print(args.string2)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "string1", type=str, help="path to first text file containing string"
+    )
+    parser.add_argument(
+        "string2", type=str, help="path to first text file containing string"
+    )
 
-with open(args.string1, 'r') as file:
-    string1 = file.read().replace('\n', '')
+    args = parser.parse_args()
+    print(args.string1)
+    print(args.string2)
 
-with open(args.string1, 'r') as file:
-    string2 = file.read().replace('\n', '')
+    with open(args.string1, "r") as file:
+        string1 = file.read().replace("\n", "")
 
-print(len(string1))
-str_arr1 = np.char.array(string1)
+    with open(args.string2, "r") as file:
+        string2 = file.read().replace("\n", "")
 
-u = 4 # A, T, C, G
+    print(len(string1))
+    str_arr1 = np.array(list(string1))
+    str_arr2 = np.array(list(string2))
+
+    # sigma = {"A": 0, "T": 1, "C": 2, "G": 3}
+    sigma = Dict.empty(key_type=types.unicode_type, value_type=types.int64)
+    sigma["A"] = 0
+    sigma["C"] = 1
+    sigma["T"] = 2
+    sigma["G"] = 3
+    s = ["A", "C", "T", "G"]
+    sigma_reverse = List()
+    [sigma_reverse.append(a) for a in s]
+    u = 4  # A, T, C, G
+    m = str_arr1.size
+    n = str_arr2.size
+    MI = np.zeros((u, n), dtype=int)
+    ED = np.zeros((m, n), dtype=int)
+    start = time.time()
+    compute_mi(MI, u, n, sigma_reverse, string2)
+    compute_edt(MI, ED, string1, m, n)
+    end = time.time()
+    print(ED[-1, -1])
+    print("Time: {}".format(end - start))
+
+
+@njit
+def compute_mi(MI, u, n, sigma, str_arr2):
+    for i in range(0, u):  # parallelize this
+        for j in range(0, n):
+            if j == 0 and sigma[i] != str_arr2[j]:
+                MI[i, j] = -1
+            elif sigma[i] == str_arr2[j]:
+                MI[i, j] = j
+            else:
+                MI[i, j] = MI[i, j - 1]
+
+
+@njit
+def compute_edt(MI, ED, str_arr1, m, n):
+    for i in range(0, m):  # str arr1
+        for j in range(0, n):  # str arr2, parallelize this
+            # mi = MI[sigma[str_arr1[i]], j]
+            if str_arr1[i] == "A":
+                lmi = MI[0, j]
+            elif str_arr1[i] == "C":
+                lmi = MI[1, j]
+            elif str_arr1[i] == "T":
+                lmi = MI[2, j]
+            else:
+                lmi = MI[3, j]
+
+            if j == 0:
+                ED[i, j] = i
+            elif i == 0:
+                ED[i, j] = j
+            elif j == lmi:  # lmi
+                ED[i, j] = ED[i - 1, j - 1]
+            elif lmi == -1:
+                ED[i, j] = min(ED[i - 1, j - 1] + 1, ED[i - 1, j] + 1)
+            # elif j < lmi:
+            else:
+                ED[i, j] = min(
+                    ED[i - 1, j - 1] + 1,
+                    ED[i - 1, j] + 1,
+                    ED[i - 1, lmi - 1] + (j - lmi),
+                )
+                # else:
+                # print("Not supposed to reach here")
+
+
+if __name__ == "__main__":
+    main()
