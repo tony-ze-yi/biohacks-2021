5f8b71c6a449a134ef5c68ae882d34f2e26638e6
Tony (Ze) Yi <24402057+tony-ze-yi@users.noreply.github.com>
2021-03-13 21:43:33 -0500

    chore: refactor code

--- main.py
+++ main.py
@@ -9,6 +9,24 @@ from numba.typed import Dict, List
 import argparse
 
 
+def similar(first, second):
+    if len(first) < len(second):
+        return similar(second, first)
+
+    if len(second) == 0:
+        return len(first)
+
+    prev_row = range(len(second) + 1)
+    for i, c1 in enumerate(first):
+        current_row = [i + 1]
+        for j, c2 in enumerate(second):
+            insertions = prev_row[j + 1] + 1
+            deletions = current_row[j] + 1
+            substitutions = prev_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+    return prev_row[-1]
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -19,8 +37,8 @@ def main():
     )
 
     args = parser.parse_args()
-    print(args.string1)
-    print(args.string2)
+    # print(args.string1)
+    # print(args.string2)
 
     with open(args.string1, "r") as file:
         string1 = file.read().replace("\n", "")
@@ -28,10 +46,18 @@ def main():
     with open(args.string2, "r") as file:
         string2 = file.read().replace("\n", "")
 
-    print(len(string1))
+    # print(len(string1))
     str_arr1 = np.array(list(string1))
     str_arr2 = np.array(list(string2))
 
+    # seq
+    start = time.time()
+    res = similar(str_arr1, str_arr2)
+    end = time.time()
+    print(res)
+    print("Time: {}".format(end - start))
+
+    # parallel
     # sigma = {"A": 0, "T": 1, "C": 2, "G": 3}
     sigma = Dict.empty(key_type=types.unicode_type, value_type=types.int64)
     sigma["A"] = 0
