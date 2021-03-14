4909bfae5f2532fc582c8185c726a2d40a1bda10
Philippe Yu <philippe.yu@mail.utoronto.ca>
2021-03-13 23:17:01 -0400

    fix: Sequential algo

--- seq-algo.py
+++ seq-algo.py
@@ -5,22 +5,23 @@ import argparse
 
 import numpy as np
 
-def similar(first, second):
-    if len(first) < len(second):
-        return similar(second, first)
+def similar(str1, str2):
+    m = len(str1)
+    n = len(str2)
 
-    if len(second) == 0:
-        return len(first)
+    dp = [[0 for x in range(n + 1)] for x in range(m + 1)]
 
-    prev_row = range(len(second) + 1)
-    for i, c1 in enumerate(first):
-        current_row = [i+1]
-        for j, c2 in enumerate(second):
-            insertions = prev_row[j + 1] + 1
-            deletions = current_row[j] + 1
-            substitutions = prev_row[j] + (c1 != c2)
-            current_row.append(min(insertions, deletions, substitutions))
-    return prev_row[-1]
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0:
+                dp[i][j] = j
+            elif j == 0:
+                dp[i][j] = i
+            elif str1[i-1] == str2[j-1]:
+                dp[i][j] = dp[i-1][j-1]
+            else:
+                dp[i][j] = 1 + min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1])
+    return dp[m][n]
 
 
 def main():
@@ -33,8 +34,6 @@ def main():
     )
 
     args = parser.parse_args()
-    print(args.string1)
-    print(args.string2)
 
     with open(args.string1, "r") as file:
         string1 = file.read().replace("\n", "")
@@ -42,7 +41,6 @@ def main():
     with open(args.string2, "r") as file:
         string2 = file.read().replace("\n", "")
 
-    print(len(string1))
     str_arr1 = np.array(list(string1))
     str_arr2 = np.array(list(string2))
 
@@ -52,3 +50,5 @@ def main():
     print(res)
     print("Time: {}".format(end - start))
 
+if __name__ == '__main__':
+    main()
